f3cd996f3724daad751e9cfd5b9f8d8b01b18f8e
Jarol <jarol.tellez228188@potros.itson.edu.mx>
2023-02-28 21:03:33 -0800

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/popcornfactory/cell_movie.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cell_movie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cell_movie)
+    }
+}
